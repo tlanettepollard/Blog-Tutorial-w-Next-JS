b0eaa551171bba6b94137062644deddfcc9b3b45
T. Lanette <TLPollard4516@gmail.com>
2023-09-30 21:42:22 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -2,15 +2,8 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/Home.module.css';
 import { getSortedPostsData } from '../lib/posts';
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
+import Link from 'next/link';
+import Date from '../components/date';
 
 
 export default function Home({ allPostsData }) {
@@ -32,11 +25,11 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
@@ -46,3 +39,11 @@ export default function Home({ allPostsData }) {
 }
 
 
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
